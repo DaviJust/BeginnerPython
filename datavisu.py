5403f2c43aa782ae88bd43e3a8cd91a6c5fd2401
Davi <davijustinocouto@gmail.com>
2023-04-21 11:26:18 -0300

    Criação do hello world e visualizador de dados: Foi necessário baixar várias extensões Biblioteca do py necessária para executar o código

---
+++ datavisu.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Random x and y values for a scatter plot 
+x = np.random.rand(50) 
+y = np.random.rand(50)
+
+# Plot the data
+plt.scatter(x, y) 
+plt.show()
